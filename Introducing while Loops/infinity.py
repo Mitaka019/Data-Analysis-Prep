b6a2f7a54fe6d7aa5353d2c94b4951884faa5fe7
Mitaka019 <157563777+Mitaka019@users.noreply.github.com>
2025-10-05 03:02:35 +0800

    Add files via upload

---
+++ Introducing while Loops/infinity.py
@@ -0,0 +1,8 @@
+
+# Introducing while Loops
+
+# 7.7
+
+x = 1
+while x <= 5:
+    print(x)
---
+++ Introducing while Loops/parrot.py
@@ -0,0 +1,43 @@
+
+# Introducing while Loops
+
+# Letting the User Choose When to Quit
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    print(message)
+
+# To remove the 'quit' message from being printed, we can use a conditional test inside the loop:
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
+# Using a Flag
+
+# This variable, called a flag, acts as a signal to the program. 
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# The output is still the same as before, but the program is structured a bit differently.
+
